1cdd01fe90628339d82a0e8063a5af0cc5f94578
hoang.tuan.anh <hoang.tuan.anh@framgias-Mac-mini.local>
2018-09-06 14:21:54 +0700

    get films from api

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,22 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
+  <div id="app" class="container">
+    <film-list></film-list>
     <router-view/>
   </div>
 </template>
 
 <script>
+
+import FilmList from './components/FilmList'
+
 export default {
   name: 'App',
+  components: {
+    FilmList
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
